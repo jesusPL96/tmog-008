7191c9cf2320b70396c0d639f1a0a29a1b7bd913
Jesús Peiró López <jpeirolo@mercadona.es>
2024-03-07 16:18:13 +0100

    feat: Solution with tests

--- src/main/java/org/tfoc/Solution.java
+++ src/main/java/org/tfoc/Solution.java
@@ -1,15 +1,33 @@
 package org.tfoc;
 
+
 /**
  * The class containing the solution to this exercise
  */
 public class Solution {
 
-    public Integer search(
-            Integer[] nums,
-            Integer target) {
+    public static int search(
+            int[] nums,
+            int target) {
 
-        return null;
+        return binarySearch(nums, target, 0, nums.length-1);
+    }
+
+    public static int binarySearch (int[] nums, int target, int start, int end){
+
+        int half = (start+end)/2;
+        if(nums[half] == target){
+            return half;
+        } else if(start>=end){
+            return -1;
+        } else {
+            if(target>nums[half]){
+                return binarySearch(nums, target, half+1, end);
+            } else{
+                return binarySearch(nums, target, start, half-1);
+            }
+        }
+
     }
 
 }
--- src/test/java/org/tfoc/SolutionTest.java
+++ src/test/java/org/tfoc/SolutionTest.java
@@ -3,13 +3,50 @@ package org.tfoc;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @Slf4j
 class SolutionTest {
 
     @Test
-    void testSolution() {
+    void testSolutionEvenLength() {
 
-        log.info("Don't forget to write the tests to make sure it works. Enjoy!");
+        int[] nums = {1, 2, 5, 7, 9, 11, 14, 18};
+        int target = 14;
+        var result = Solution.search(nums, target);
+
+        assertEquals(6, result);
     }
+
+    @Test
+    void testSolutionOddLength() {
+
+        int[] nums = {1, 2, 5, 7, 9, 11, 14, 18, 21};
+        int target = 14;
+        var result = Solution.search(nums, target);
+
+        assertEquals(6, result);
+    }
+
+    @Test
+    void testNotFoundOnArray() {
+
+        int[] nums = {1, 2, 5, 7, 9, 11, 14, 18, 21};
+        int target = 4;
+        var result = Solution.search(nums, target);
+
+        assertEquals(-1, result);
+    }
+
+    @Test
+    void testLengthOfOne() {
+
+        int[] nums = {1};
+        int target = 1;
+        var result = Solution.search(nums, target);
+
+        assertEquals(0, result);
+    }
+
 
 }
